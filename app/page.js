3666b673c6ee410b3718ed6eaf4759637077eec8
Mrinal <prashar.mrinal8888@gmail.com>
2026-01-03 02:49:18 +0530

    Refine marquee font size and update branding text

--- app/page.js
+++ app/page.js
@@ -432,28 +432,25 @@ const page = () => {
           <div className="marquee-inner to-left">
             <ul className="marqee-list d-flex">
               <li className="marquee-item">
-                <span className="text-slider">Security</span>
-                <span className="text-slider">Guards </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Bouncer</span>
-                <span className="text-slider">Services </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Escort</span>
-                <span className="text-slider">Guards </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Facility </span>
-                <span className="text-slider">Management</span>
-                <span className="text-slider">Security</span>
-                <span className="text-slider">Guards </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Bouncer</span>
-                <span className="text-slider">Services </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Escort</span>
-                <span className="text-slider">Guards </span>
-                <span className="text-slider style-border" />
-                <span className="text-slider">Facility </span>
-                <span className="text-slider">Management</span>
+                <span className="text-slider" style={{ fontSize: "70px" }}>IBSM Global Security</span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Security Guards </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Bouncer Services </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Escort Guards </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Facility Management</span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>IBSM Global Security</span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Security Guards </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Bouncer Services </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Escort Guards </span>
+                <span className="text-slider style-border" style={{ fontSize: "70px" }} />
+                <span className="text-slider" style={{ fontSize: "70px" }}>Facility Management</span>
               </li>
             </ul>
           </div>
